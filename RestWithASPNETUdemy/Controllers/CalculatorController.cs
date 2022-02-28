e3b6c1fe4fea3c6aa2515fb1c29ee46da38eed3e
Roberto Sousa <robert6257@gmail.com>
2022-02-28 11:05:18 -0300

    Add function Sum

--- RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -15,6 +15,17 @@ namespace RestWithASPNETUdemy.Controllers
             _logger = logger;
         }
 
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
         private decimal ConvertToDecimal(string strNumber)
         {
             decimal decimalValue;
