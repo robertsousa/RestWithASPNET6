83b70e88fd4959b0c0467ac18f8d21360f93b35b
Roberto Sousa <robert6257@gmail.com>
2022-02-28 11:46:47 -0300

    Add and implement a PersonController

--- RestWithASPNETUdemy/Program.cs
+++ RestWithASPNETUdemy/Program.cs
@@ -1,7 +1,9 @@
+using RestWithASPNETUdemy.Services.Implementation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();  
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
