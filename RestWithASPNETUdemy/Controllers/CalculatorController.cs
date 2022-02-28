951a9bf91969d39d985b295c3ba1caacfce72282
Roberto Sousa <robert6257@gmail.com>
2022-02-28 11:07:06 -0300

    Add function Mean

--- RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -59,6 +59,16 @@ namespace RestWithASPNETUdemy.Controllers
             return BadRequest("Invalid input");
         }
 
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(mean.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
 
 
 
